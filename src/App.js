073cbf11e1d3bdf0411ef5db4338236cac5c8e14
ant-shubhamkaushal <shubham.kaushal@antcreatives.com>
2025-01-07 11:33:45 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Star from "./components/Star";
+import Check from "./components/Checked/Check";
 
 function App() {
   return (
     <div className="">
-      <Star />
+      {/* <Star /> */}
+      <Check />
     </div>
   );
 }
