4efe4cc5c118e5732b6c6d959fead81fe25671a5
ant-shubhamkaushal <shubham.kaushal@antcreatives.com>
2025-01-11 13:19:09 +0530

    multi_form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Star from "./components/Star";
+import Star from "./components/star/Star";
 import Check from "./components/Checked/Check";
+import Form from "./components/multi_form/Form";
 
 function App() {
   return (
     <div className="">
       {/* <Star /> */}
-      <Check />
+      {/* <Check /> */}
+      <div className="h-screen bg-pink-200">
+        <Form />
+      </div>
     </div>
   );
 }
