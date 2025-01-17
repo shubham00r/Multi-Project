b02c5e3a0352ce7a2bfda3c2cc55db584f049930
ant-shubhamkaushal <shubham.kaushal@antcreatives.com>
2025-01-17 17:32:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Star from "./components/star/Star";
 import Check from "./components/Checked/Check";
 import Form from "./components/multi_form/Form";
 import Search from "./components/Filter_Search/Search";
+import ExpenseTracker from "./components/Expensive/ExpensiveTracker";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <div className="h-screen bg-pink-200">
         <Form />
       </div> */}
-      <Search />
+      <ExpenseTracker />
+      {/* <Search /> */}
     </div>
   );
 }
